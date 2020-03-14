16e176777c8694e20c930f843bb3de2da9ce4450
Pinchuk Yevhen <jagernaut1233@gmail.com>
2020-03-14 18:53:14 +0200

    Initial commit

---
+++ src/main/java/com/pinchuk/yevhen/msccbeerservice/MsccBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pinchuk.yevhen.msccbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsccBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsccBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pinchuk/yevhen/msccbeerservice/MsccBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinchuk.yevhen.msccbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsccBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
