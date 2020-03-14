bd47a43aa1ee8eba1a65dccb349c7c5530090a6b
Pinchuk Yevhen <jagernaut1233@gmail.com>
2020-03-14 19:32:35 +0200

    added beer controller

---
+++ src/main/java/com/pinchuk/yevhen/msccbeerservice/web/controller/BeerController.java
@@ -0,0 +1,34 @@
+package com.pinchuk.yevhen.msccbeerservice.web.controller;
+
+import com.pinchuk.yevhen.msccbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+/**
+ * @author Pinchuk Yevhen
+ * @created 14/03/2020 - 19:25
+ */
+@RestController
+@RequestMapping("api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID beerId){
+        return new ResponseEntity(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveBeer(@RequestBody BeerDto beerDto){
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeer(@PathVariable UUID beerId, @RequestBody BeerDto beerDto){
+        return  new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    
+}
